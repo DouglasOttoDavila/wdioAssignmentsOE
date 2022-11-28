6fd40d6c57f62f033a289d5d572cc7eb757d9835
DouglasOttoSf <douglas.davila@objectedge.com>
2022-11-28 16:25:26 -0300

    Updating Allure Reporting Tool

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,6 @@
+// wdio.conf.js
+const allure = require('allure-commandline')
+
 exports.config = {
     //
     // ====================
@@ -135,10 +138,15 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
-
-
-    
+    reporters:['spec', 'allure'],
+                reporterOptions: {
+                    allure: {
+                        outputDir: 'allure-results',
+                        disableWebdriverStepsReporting: true,
+                        disableWebdriverScreenshotsReporting: false,
+                        useCucumberStepReporter: false
+                    }
+                },
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
@@ -286,6 +294,27 @@ exports.config = {
      */
     // onComplete: function(exitCode, config, capabilities, results) {
     // },
+    onComplete: function() {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    }
+
     /**
     * Gets executed when a refresh happens.
     * @param {String} oldSessionId session ID of the old session
